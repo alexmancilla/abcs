13f6d02417751b6da2637dbaf55f53a54f56086e
alexmancilla <57542953+alexmancilla@users.noreply.github.com>
2022-09-28 16:06:54 -0500

    Create delete_duplicated_values.py
    You are given the pointer to the head node of a sorted linked list, where the data in the nodes is in ascending order. Delete nodes and return a sorted list with each distinct value in the original list. The given head pointer may be null indicating that the list is empty.

---
+++ 4 Linked Lists/delete_duplicated_values.py
@@ -0,0 +1,9 @@
+def removeDuplicates(llist):
+    # Write your code here
+    node = llist
+    while node.next:
+        if node.data == node.next.data:
+            node.next = node.next.next
+        else:
+            node = node.next
+    return llist
